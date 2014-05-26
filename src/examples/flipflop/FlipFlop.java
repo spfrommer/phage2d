d4e09f55e1c2ca924e19944325110dea7c580d18
Samuel Pfrommer <sam.pfrommer@gmail.com>
2014-05-26 13:35:25 -0400

    Changed logo and reformatted FlipFlop...

--- src/examples/flipflop/FlipFlop.java
+++ src/examples/flipflop/FlipFlop.java
@@ -99,32 +99,20 @@ public class FlipFlop extends Game {
 
 	@Override
 	public void onInit() {
-		m_waterDrop = new SoundResource(
-				SoundTest.class.getResource("/sounds/waterdrop.mp3"),
-				new GenericDecoder());
-		m_wind = new SoundResource(
-				SoundTest.class.getResource("/sounds/wind.mp3"),
-				new GenericDecoder());
-		m_rain = new SoundResource(
-				SoundTest.class.getResource("/sounds/rain.mp3"),
-				new GenericDecoder());
-		m_techno = new SoundResource(
-				SoundTest.class.getResource("/sounds/techno.wav"),
-				new GenericDecoder());
-		m_city = new SoundResource(
-				SoundTest.class.getResource("/sounds/city.mp3"),
-				new GenericDecoder());
-		m_violin = new SoundResource(
-				SoundTest.class.getResource("/sounds/violin.mp3"),
-				new GenericDecoder());
+		m_waterDrop = new SoundResource(SoundTest.class.getResource("/sounds/waterdrop.mp3"), new GenericDecoder());
+		m_wind = new SoundResource(SoundTest.class.getResource("/sounds/wind.mp3"), new GenericDecoder());
+		m_rain = new SoundResource(SoundTest.class.getResource("/sounds/rain.mp3"), new GenericDecoder());
+		m_techno = new SoundResource(SoundTest.class.getResource("/sounds/techno.wav"), new GenericDecoder());
+		m_city = new SoundResource(SoundTest.class.getResource("/sounds/city.mp3"), new GenericDecoder());
+		m_violin = new SoundResource(SoundTest.class.getResource("/sounds/violin.mp3"), new GenericDecoder());
 	}
 
 	@Override
 	public void initProcesses() {
 		m_physics = new PhysicsActivity(this.getEntitySystem());
 		m_animation = new AnimationActivity(this.getEntitySystem());
-		m_camera = new KeyboardCameraActivity(this.getEntitySystem(),
-				LWJGLKeyboard.instance(), new MovementProfile(10, 0.01));
+		m_camera = new KeyboardCameraActivity(this.getEntitySystem(), LWJGLKeyboard.instance(), new MovementProfile(10,
+				0.01));
 		m_controller = new ControllerActivity(this.getEntitySystem());
 	}
 
@@ -159,26 +147,25 @@ public class FlipFlop extends Game {
 		this.getViewPort().getCamera().setZoom(DEFAULT_CAM_ZOOM);
 		startNextLevel();
 
-		portalManager
-				.addPortalsSatisfiedListener(new PortalsSatisfiedListener() {
+		portalManager.addPortalsSatisfiedListener(new PortalsSatisfiedListener() {
+			@Override
+			public void portalsSatisfied() {
+				Thread t = new Thread(new Runnable() {
 					@Override
-					public void portalsSatisfied() {
-						Thread t = new Thread(new Runnable() {
-							@Override
-							public void run() {
-								try {
-									Thread.sleep(1000);
-								} catch (InterruptedException e) {
-									e.printStackTrace();
-								}
-								m_goToNextLevel = true;
-							}
-						});
-						t.start();
+					public void run() {
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						m_goToNextLevel = true;
 					}
 				});
-		ParallaxRenderingActivity rendering = new ParallaxRenderingActivity(
-				this.getEntitySystem(), this.getViewPort().getCamera());
+				t.start();
+			}
+		});
+		ParallaxRenderingActivity rendering = new ParallaxRenderingActivity(this.getEntitySystem(), this.getViewPort()
+				.getCamera());
 		rendering.loadEntities();
 		this.setRenderingActivity(rendering);
 		makeInputManager();
@@ -244,8 +231,7 @@ public class FlipFlop extends Game {
 			player.add(input);
 			player.addListener(new PlaylistPlayerListener() {
 				@Override
-				public void streamEnded(PlaylistPlayer player,
-						AudioInputStream done) {
+				public void streamEnded(PlaylistPlayer player, AudioInputStream done) {
 					try {
 						player.add(resource.openStream());
 					} catch (IOException e) {
@@ -254,13 +240,12 @@ public class FlipFlop extends Game {
 				}
 
 				@Override
-				public void startedPlaying(PlaylistPlayer player,
-						AudioInputStream n) {
+				public void startedPlaying(PlaylistPlayer player, AudioInputStream n) {
 
 				}
 
 			});
-			player.start();
+			// player.start();
 			return player;
 		} catch (IOException e1) {
 			e1.printStackTrace();
@@ -286,15 +271,14 @@ public class FlipFlop extends Game {
 		m_physics.setGravity(new Vector(0, -9.8));
 		portalManager.resetPortals();
 
-		int world = (int) (m_nextLevel / 3);
+		int world = m_nextLevel / 3;
 		loadWorld(world);
 		if (!LevelReader.levelExists("level" + m_nextLevel + ".lvl")) {
 			fadingMessage("You Win!");
 			return;
 		}
 
-		fadingMessage("World " + (world + 1) + " Level "
-				+ (m_nextLevel % 3 + 1));
+		fadingMessage("World " + (world + 1) + " Level " + (m_nextLevel % 3 + 1));
 		DynamicLevel dynamic = new DynamicLevel();
 		try {
 			dynamic.load(LevelReader.read("level" + m_nextLevel + ".lvl"));
@@ -366,9 +350,7 @@ public class FlipFlop extends Game {
 			try {
 				if (font == null) {
 					font = BMFontXMLLoader.loadFonts(
-							new File(FlipFlop.class.getResource(
-									"/themes/basic/ptsans.fnt").toURI()))
-							.get(0);
+							new File(FlipFlop.class.getResource("/themes/basic/ptsans.fnt").toURI())).get(0);
 				}
 				renderer.setFont(font);
 			} catch (SAXException e) {
@@ -381,18 +363,15 @@ public class FlipFlop extends Game {
 				e.printStackTrace();
 			}
 			renderer.setColor(new Color(1, 1, 1, (float) m_fade));
-			renderer.drawString(string, (int) m_position.getX(),
-					(int) m_position.getY());
+			renderer.drawString(string, (int) m_position.getX(), (int) m_position.getY());
 		}
 	}
 
 	private boolean velocitiesZero() {
 		SystemAspectManager manager = this.getEntitySystem().getAspectManager();
 		manager.loadAspect(new Aspect(TypeManager.getType(PhysicsData.class)));
-		for (Entity e : manager.getEntities(new Aspect(TypeManager
-				.getType(PhysicsData.class)))) {
-			PhysicsData data = (PhysicsData) e.getComponent(TypeManager
-					.getType(PhysicsData.class));
+		for (Entity e : manager.getEntities(new Aspect(TypeManager.getType(PhysicsData.class)))) {
+			PhysicsData data = (PhysicsData) e.getComponent(TypeManager.getType(PhysicsData.class));
 			if (data.getVelocity().length() > 150)
 				return false;
 		}
@@ -405,18 +384,12 @@ public class FlipFlop extends Game {
 		LWJGLMouse mouse = new LWJGLMouse(port.getViewShape());
 
 		final InputManager manager = new InputManager();
-		manager.addBinding("Up", new KeyTrigger(LWJGLKeyboard.instance()
-				.getKey('i')));
-		manager.addBinding("Down", new KeyTrigger(LWJGLKeyboard.instance()
-				.getKey('k')));
-		manager.addBinding("Right", new KeyTrigger(LWJGLKeyboard.instance()
-				.getKey('l')));
-		manager.addBinding("Left", new KeyTrigger(LWJGLKeyboard.instance()
-				.getKey('j')));
-		manager.addBinding("Print", new KeyTrigger(LWJGLKeyboard.instance()
-				.getKey('=')));
-		manager.addBinding("Restart", new KeyTrigger(LWJGLKeyboard.instance()
-				.getKey('r')));
+		manager.addBinding("Up", new KeyTrigger(LWJGLKeyboard.instance().getKey('i')));
+		manager.addBinding("Down", new KeyTrigger(LWJGLKeyboard.instance().getKey('k')));
+		manager.addBinding("Right", new KeyTrigger(LWJGLKeyboard.instance().getKey('l')));
+		manager.addBinding("Left", new KeyTrigger(LWJGLKeyboard.instance().getKey('j')));
+		manager.addBinding("Print", new KeyTrigger(LWJGLKeyboard.instance().getKey('=')));
+		manager.addBinding("Restart", new KeyTrigger(LWJGLKeyboard.instance().getKey('r')));
 
 		manager.addBindingListener("Up", new BindingListener() {
 			@Override
@@ -490,8 +463,9 @@ public class FlipFlop extends Game {
 	}
 
 	private void playBallSound() {
+		System.out.println("Playing sound");
 		try {
-			SoundSystem.s_getPlayer(m_waterDrop, 0.5f).start();
+			SoundSystem.s_getPlayer(m_waterDrop, 1f).start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
